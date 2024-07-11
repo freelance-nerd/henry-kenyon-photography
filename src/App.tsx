3eccbba025cc5ebb605228831ff99f13894e9fae
autumn <autumn@pop-os.localdomain>
2024-07-11 17:05:16 +0100

    added images and hover behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,33 @@ function App() {
 
   return (
     <>
-      <h1 class="font-custom">Henry Kenyon</h1>
+      <div className="max-w-screen-md">
+        <Navbar/>
+        <div className = "container columns-2 content-center">
+            <img className="rotate-12 hover:rotate-0 m-10 shadow-2xl" src = "src/assets/wraa.jpg"/>
+            <img className="rotate-6 hover:rotate-0 m-10 shadow-2xl" src = "src/assets/u15future.jpg"/>
+            <a link="google.com">
+              <img className="-rotate-12 hover:rotate-12 m-10 shadow 2xl" src = "src/assets/lots.jpg"/>
+            </a>
+        </div>
+        <Footer/>
+      </div>
     </>
   )
 }
+
+function Navbar() {
+  return <h1 className = "text-7xl font-label shadow-3xl">Henry Kenyon Photography</h1>
+}
+
+function Footer() {
+  return (
+    <div className="object-center">
+      footer
+    </div>
+  )
+}
+
+
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        custom: ["ImpactLabel", "sans-serif"],
+        label: ["ImpactLabel", "sans-serif"],
       }
     },
   },
